4d00d5bb5bc7e79670f468398a7cd01643c312f8
Johannes Finnsson <johannes.finnsson@gmail.com>
2011-02-19 16:52:41 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Radicase::Application.config.secret_token = '3b53ee5486e3f840d72742325b5b5f3c20ea2264f8cec71e4db458e1e9a6a19b4ab5c7d07786c91117f786f3af2af57a0e3580c7f994a155bb36e08e72d18008'
